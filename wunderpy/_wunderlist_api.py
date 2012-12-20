7dd5a0f9d99e784d6a8d1824ee65a70f44079609
bsmt <push@Solid.local>
2012-12-19 18:02:05 -0700

    pep8

--- wunderpy/_wunderlist_api.py
+++ wunderpy/_wunderlist_api.py
@@ -2,14 +2,14 @@ import requests
 
 
 class API(object):
-    def __init__(self, url='https://api.wunderlist.com', request_timeout=60):
+    def __init__(self, url='https://api.wunderlist.com', request_timeout=30):
         self.api_url = url
         self.timeout = request_timeout
 
     def login(self, email, password):
         login_url = self.api_url + '/login'
         request_body = {"email": email, "password": password}
-        
+
         login_request = requests.post(login_url, data=request_body,
                                       timeout=self.timeout)
         if login_request.status_code == 200:  # All good
