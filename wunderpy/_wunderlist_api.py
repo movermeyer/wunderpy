8d53a144f35702b36d13fb83ee70c280557e6df5
bsmt <push@Solid.local>
2012-12-23 13:52:26 -0700

    Add timeout values to API calls.

--- wunderpy/_wunderlist_api.py
+++ wunderpy/_wunderlist_api.py
@@ -34,7 +34,8 @@ class API(object):
 
     def me(self):
         me_url = self.api_url + "/me"
-        me_request = requests.get(me_url, headers=self.header)
+        me_request = requests.get(me_url, headers=self.header,
+                                  timeout=self.timeout)
 
         if me_request.status_code == 200:
             return me_request.json()
@@ -43,7 +44,8 @@ class API(object):
 
     def get_all_tasks(self):
         tasks_url = self.api_url + "/me/tasks"
-        tasks_request = requests.get(tasks_url, headers=self.header)
+        tasks_request = requests.get(tasks_url, headers=self.header,
+                                     timeout=self.timeout)
 
         if tasks_request.status_code == 200:
             return tasks_request.json()
@@ -61,7 +63,8 @@ class API(object):
         if due_date:
             body["due_date"] = due_date  # should be in ISO format
         tasks_request = requests.post(tasks_url, data=body,
-                                      headers=self.header)
+                                      headers=self.header,
+                                      timeout=self.timeout)
 
         if tasks_request.status_code == 201:  # Created
             return tasks_request.json()
@@ -72,7 +75,8 @@ class API(object):
         task_url = self.api_url + "/" + task_id
         body = {"note": note}
         task_request = requests.put(task_url, data=body,
-                                    headers=self.header)
+                                    headers=self.header,
+                                    timeout=self.timeout)
 
         if task_request.status_code == 200:
             return task_request.json()  # new task info
@@ -81,7 +85,8 @@ class API(object):
 
     def delete_task(self, task_id):
         task_url = self.api_url + "/" + task_id
-        task_request = requests.delete(task_url, headers=self.header)
+        task_request = requests.delete(task_url, headers=self.header,
+                                       timeout=self.timeout)
 
         if task_request.status_code == 200:
             return True  # server doesn't respond with anything
@@ -90,7 +95,8 @@ class API(object):
 
     def get_lists(self):
         lists_url = self.api_url + "/me/lists"
-        lists_request = requests.get(lists_url, headers=self.header)
+        lists_request = requests.get(lists_url, headers=self.header,
+                                     timeout=self.timeout)
 
         if lists_request.status_code == 200:
             return lists_request.json()
@@ -101,7 +107,8 @@ class API(object):
         lists_url = self.api_url + "/me/lists"
         body = {"title": list_name}
         list_request = requests.post(lists_url, data=body,
-                                     headers=self.header)
+                                     headers=self.header,
+                                     timeout=self.timeout)
 
         if list_request.status_code == 201:
             return list_request.json()
@@ -110,7 +117,8 @@ class API(object):
 
     def delete_list(self, list_id):
         list_url = self.api_url + "/" + list_id
-        list_request = requests.delete(list_url, headers=self.header)
+        list_request = requests.delete(list_url, headers=self.header,
+                                       timeout=self.timeout)
 
         if list_request.status_code == 200:
             return True
@@ -119,7 +127,8 @@ class API(object):
 
     def get_reminders(self):
         reminders_url = self.api_url + "/me/reminders"
-        reminders_request = requests.get(reminders_url, headers=self.header)
+        reminders_request = requests.get(reminders_url, headers=self.header,
+                                         timeout=self.timeout)
 
         if reminders_request.status_code == 200:
             return reminders_request.json()
@@ -129,7 +138,8 @@ class API(object):
 
     def get_shares(self):
         shares_url = self.api_url + "/me/shares"
-        shares_request = requests.get(shares_url, headers=self.header)
+        shares_request = requests.get(shares_url, headers=self.header,
+                                      timeout=self.timeout)
 
         if shares_request.status_code == 200:
             return shares_request.json()
@@ -138,7 +148,8 @@ class API(object):
 
     def get_services(self):
         services_url = self.api_url + "/me/services"
-        services_request = requests.get(services_url, headers=self.header)
+        services_request = requests.get(services_url, headers=self.header,
+                                        timeout=self.timeout)
 
         if services_request.status_code == 200:
             return services_request.json()
@@ -147,7 +158,8 @@ class API(object):
 
     def get_events(self):
         events_url = self.api_url + "/me/events"
-        events_request = requests.get(events_url, headers=self.header)
+        events_request = requests.get(events_url, headers=self.header,
+                                      timeout=self.timeout)
 
         if events_request.status_code == 200:
             return events_request.json()
@@ -156,7 +168,8 @@ class API(object):
 
     def get_settings(self):
         settings_url = self.api_url + "/me/settings"
-        settings_request = requests.get(settings_url, headers=self.header)
+        settings_request = requests.get(settings_url, headers=self.header,
+                                        timeout=self.timeout)
 
         if settings_request.status_code == 200:
             return settings_request.json()
@@ -165,7 +178,8 @@ class API(object):
 
     def get_friends(self):
         friends_url = self.api_url + "/me/friends"
-        friends_request = requests.get(friends_url, headers=self.header)
+        friends_request = requests.get(friends_url, headers=self.header,
+                                       timeout=self.timeout)
 
         if friends_request.status_code == 200:
             return friends_request.json()
