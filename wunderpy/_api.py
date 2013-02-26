51fb5f7ffff5f41f84c042b62031666b94082b16
bsmt <bsmt@krax.in>
2013-02-25 23:21:04 -0700

    All API methods so far are documented, sparsely…

--- wunderpy/_api.py
+++ wunderpy/_api.py
@@ -1,6 +1,5 @@
 '''
 .. module:: _api
-    :synopsis: API calls to Wunderlist.
 '''
 
 import requests
@@ -11,15 +10,21 @@ class API(object):
     
     The methods in this class simply send requests to Wunderlist.
     It's meant to only be used by the :class:`Wunderlist` class.
+    
+    .. note::
+        All requests (except for login) require an authentication header
+        using a token received from the login request.
+        The values to go into the header are {"Authorization": "Bearer " + token}.
+
+    .. |url| replace:: API_URL
     '''
 
     def __init__(self, url="https://api.wunderlist.com", request_timeout=30):
         '''
         :param url: URL of the API that will be used.
-        :type url: str.
-
+        :type url: str
         :param request_timeout: Timeout value (seconds) for all requests.
-        :type request_timeout: int.
+        :type request_timeout: int
         '''
 
         self.api_url = url
@@ -28,13 +33,11 @@ class API(object):
 
     def login(self, email, password):
         '''Login to Wunderlist.
-        
-        :param email: The account's email address.
-        :type email: str.
 
+        :param email: The account's email address.
+        :type email: str
         :param password: The account's password.
-        :type password: str.
-        
+        :type password: str
         :returns: bool -- Whether the login was successful.
         :raises: Exception
         '''
@@ -55,6 +58,12 @@ class API(object):
             raise Exception("Login error", login_request.status_code)
 
     def me(self):
+        '''Get info about the logged in user
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         me_url = self.api_url + "/me"
         me_request = requests.get(me_url, headers=self.header,
                                   timeout=self.timeout)
@@ -65,6 +74,12 @@ class API(object):
             raise Exception("Get me error", me_request.status_code)
 
     def get_all_tasks(self):
+        '''Get every task associated with the account.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         tasks_url = self.api_url + "/me/tasks"
         tasks_request = requests.get(tasks_url, headers=self.header,
                                      timeout=self.timeout)
@@ -75,6 +90,20 @@ class API(object):
             raise Exception("Get tasks error", tasks_request.status_code)
 
     def add_task(self, title, list_id, due_date=None, starred=False):
+        '''Add a task to a list.
+        
+        :param title: The task name/title.
+        :type title: str
+        :param list_id: id of the list to put the task in.
+        :type list_id: str
+        :param due_date: Date the task will be due in ISO format
+        :type due_date: str
+        :param starred: Whether the task should be starred.
+        :type starred: bool
+        :returns: dict
+        :raises: Exception
+        '''
+
         if starred is False:
             starred = 0
         elif starred is True:
@@ -94,6 +123,16 @@ class API(object):
             raise Exception("Add task error", tasks_request.status_code)
 
     def set_note_for_task(self, note, task_id):
+        '''Set a task's note field.
+        
+        :param note: The note's contents.
+        :type note: str
+        :param task_id: The id of the task.
+        :type task_id: str
+        :returns: dict
+        :raises: Exception
+        '''
+
         task_url = self.api_url + "/" + task_id
         body = {"note": note}
         task_request = requests.put(task_url, data=body,
@@ -106,6 +145,18 @@ class API(object):
             raise Exception("Set task note error", task_request.status_code)
 
     def set_task_due_date(self, task_id, due_date, recurrence_count=1):
+        '''Set a task's due date.
+        
+        :param task_id: The id of the task.
+        :type task_id: str
+        :param due_date: The new due date in ISO format.
+        :type due_date: str
+        :param recurrence_count: Not completely sure yet.
+        :type recurrence_count: int
+        :returns: dict
+        :raises: Exception
+        '''
+
         task_url = self.api_url + "/" + task_id
         body = {"due_date": due_date, "recurrence_count": recurrence_count}
         task_request = requests.put(task_url, data=body,
@@ -118,6 +169,14 @@ class API(object):
             raise Exception("Set task due date error")
 
     def delete_task(self, task_id):
+        '''Delete a task.
+        
+        :param task_id: The task's id.
+        :type task_id: str
+        :returns: dict
+        :raises: Exception
+        '''
+
         task_url = self.api_url + "/" + task_id
         task_request = requests.delete(task_url, headers=self.header,
                                        timeout=self.timeout)
@@ -128,6 +187,12 @@ class API(object):
             raise Exception("Delete task error", task_request.status_code)
 
     def get_lists(self):
+        '''Get all of the task lists
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         lists_url = self.api_url + "/me/lists"
         lists_request = requests.get(lists_url, headers=self.header,
                                      timeout=self.timeout)
@@ -138,6 +203,14 @@ class API(object):
             raise Exception("Get lists error", lists_request.status_code)
 
     def add_list(self, list_name):
+        '''Create a new task list.
+        
+        :param list_name: The name of the new list.
+        :type list_name: str
+        :returns: dict
+        :raises: Exception
+        '''
+
         lists_url = self.api_url + "/me/lists"
         body = {"title": list_name}
         list_request = requests.post(lists_url, data=body,
@@ -150,6 +223,14 @@ class API(object):
             raise Exception("Add list error", list_request.status_code)
 
     def delete_list(self, list_id):
+        '''Delete a list and all of its contents.
+        
+        :param list_id: The id of the list to delete.
+        :type list_id: str
+        :returns: dict
+        :raises: Exception
+        '''
+
         list_url = self.api_url + "/" + list_id
         list_request = requests.delete(list_url, headers=self.header,
                                        timeout=self.timeout)
@@ -160,6 +241,12 @@ class API(object):
             raise Exception("Delete list error", list_request.status_code)
 
     def get_reminders(self):
+        '''Get a list of all reminders.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         reminders_url = self.api_url + "/me/reminders"
         reminders_request = requests.get(reminders_url, headers=self.header,
                                          timeout=self.timeout)
@@ -171,6 +258,14 @@ class API(object):
                             reminders_request.status_code)
 
     def set_reminder_for_task(self, task_id, date):
+        '''Add a reminder for a task.
+        
+        :param task_id: The id of the task.
+        :type task_id: str
+        :param date: The reminder date/time in ISO format.
+        :type date: str
+        '''
+
         reminders_url = self.api_url + "/me/reminders"
         body = {"task_id": task_id, "date": date}  # date is in ISO date format
         reminders_request = requests.post(reminders_url, headers=self.header,
@@ -183,6 +278,12 @@ class API(object):
                             reminders_request.status_code)
 
     def get_shares(self):
+        '''Get a list of all things shared with you, I think...
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         shares_url = self.api_url + "/me/shares"
         shares_request = requests.get(shares_url, headers=self.header,
                                       timeout=self.timeout)
@@ -193,6 +294,12 @@ class API(object):
             raise Exception("Get shares error", shares_request.status_code)
 
     def get_services(self):
+        '''Not sure.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         services_url = self.api_url + "/me/services"
         services_request = requests.get(services_url, headers=self.header,
                                         timeout=self.timeout)
@@ -203,6 +310,12 @@ class API(object):
             raise Exception("Get services error", services_request.status_code)
 
     def get_events(self):
+        '''Not sure.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         events_url = self.api_url + "/me/events"
         events_request = requests.get(events_url, headers=self.header,
                                       timeout=self.timeout)
@@ -213,6 +326,12 @@ class API(object):
             raise Exception("Get events error", events_request.status_code)
 
     def get_settings(self):
+        '''Get account settings.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         settings_url = self.api_url + "/me/settings"
         settings_request = requests.get(settings_url, headers=self.header,
                                         timeout=self.timeout)
@@ -223,6 +342,12 @@ class API(object):
             raise Exception("Get settings error", settings_request.status_code)
 
     def get_friends(self):
+        '''Get friends list.
+        
+        :returns: dict
+        :raises: Exception
+        '''
+
         friends_url = self.api_url + "/me/friends"
         friends_request = requests.get(friends_url, headers=self.header,
                                        timeout=self.timeout)
