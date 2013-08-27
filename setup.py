2de7df606a005d926570e71ec0a46d35c130d0cd
bsmt <bsmt@krax.in>
2013-08-26 18:47:30 -0600

    I'm really not good with version numbers.

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 setup(
     name="wunderpy",
-    version="0.1",
-    packages=find_packages(),
+    version="0.2",
+    packages=find_packages(exclude=["tests", "tests.*"]),
     install_requires=["requests>=1.1.0"],
     url="https://github.com/bsmt/wunderpy"
 )
