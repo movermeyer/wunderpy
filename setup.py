11cbfb50b5f27cdfcf3fa330bf1e1ddc57aea847
bsmt <bsmt@krax.in>
2013-09-18 10:35:20 -0600

    Make setup.py more friendly

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+
 setup(
     name="wunderpy",
-    version="0.2",
-    packages=find_packages(exclude=["tests", "tests.*"]),
-    install_requires=["requests>=1.1.0"],
-    url="https://github.com/bsmt/wunderpy"
+    version="0.2.0",
+    author="bsmt",
+    author_email="bsmt@krax.in",
+    url="https://github.com/bsmt/wunderpy",
+    license="LICENSE",
+    description="An experimental wrapper for the Wunderlist 2 API",
+    long_description=open("README.md").read(),
+    packages=find_packages(),
+    install_requires=required
 )
