4f65e3ff92677a80f27ee2d182befcbf12584ff6
bsmt <bsmt@bsmt.me>
2013-11-22 16:18:52 -0700

    Fix huge setup.py fail

--- setup.py
+++ setup.py
@@ -1,22 +1,29 @@
 from setuptools import setup, find_packages
 
 try:
-    desc_file = open("README.md")
+    desc_file = open("README.rst")
     description = desc_file.read()
+    desc_file.close()
 except:
     description = ""
 
 
 setup(
     name="wunderpy",
-    version="0.2.0",
+    version="0.2.1",
     author="bsmt",
     author_email="bsmt@bsmt.me",
     url="https://github.com/bsmt/wunderpy",
-    license="LICENSE",
+    license="MIT",
     description="An experimental wrapper for the Wunderlist 2 API",
     long_description=description,
+    classifiers=['Development Status :: 4 - Beta',
+                'License :: OSI Approved :: MIT License',
+                'Programming Language :: Python',
+                'Topic :: Utilities',
+                'Topic :: Documentation'
+                ],
     packages=find_packages(exclude=("tests",)),
     install_requires=["requests>=1.1.0"],
-    entry_points={'console_scripts':['wunderlist = wunderpy.cli.main:main']}
+    entry_points={'console_scripts': ['wunderlist = wunderpy.cli.main:main']}
 )
